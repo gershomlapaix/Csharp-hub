e92332e2274f7e36e9ae541470bb6bcf7741a2b8
gershomlapaix <nsengiyumv33@gmail.com>
2023-07-27 22:15:12 +0200

    async with console app

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Program
+{
+    static void Main()
+    {
+        var lines = ReadFrom("sample.txt");
+        foreach (var line in lines)
+        {
+            Console.Write(line);
+            if (!string.IsNullOrWhiteSpace(line))
+            {
+                var pause = Task.Delay(100);
+                // Synchronously waiting on a task is an
+                // anti-pattern. This will get fixed in later
+                // steps.
+                pause.Wait();
+            }
+        }
+    }
+
+    static IEnumerable<string> ReadFrom(string file)
+    {
+        string? line;
+        using (var reader = File.OpenText(file))
+        {
+            while ((line = reader.ReadLine()) != null)
+            {
+                var words = line.Split(' ');
+                foreach (var word in words)
+                {
+                    yield return word + " ";
+                }
+                yield return Environment.NewLine;
+            }
+        }
+    }
+}
