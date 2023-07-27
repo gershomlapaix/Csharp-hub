bb7d2fcafec27802ad0363cb0f83b584986be97b
gershomlapaix <nsengiyumv33@gmail.com>
2023-07-27 19:48:09 +0200

    finish

---
+++ NullableIntroduction/Program.cs
@@ -0,0 +1,16 @@
+﻿using NullableIntroduction;
+
+public
+class Program
+{
+    static void Main()
+    {
+        var surveyRun = new SurveyRun();
+        surveyRun.AddQuestion(QuestionType.YesNo, "Has your code ever thrown a NullReferenceException?");
+        surveyRun.AddQuestion(new SurveyQuestion(QuestionType.Number, "How many times (to the nearest 100) has that happened?"));
+        surveyRun.AddQuestion(QuestionType.Text, "What is your favorite color?");
+        surveyRun.AddQuestion(QuestionType.Text, default); // should show a warning
+
+        surveyRun.PerformSurvey(50);
+    }
+}
