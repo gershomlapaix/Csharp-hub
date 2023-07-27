87abc66261e94dbf017fa18e59245819c12f773b
gershomlapaix <nsengiyumv33@gmail.com>
2023-07-27 13:07:45 +0200

    using discards

---
+++ discards/Program.cs
@@ -0,0 +1,57 @@
+﻿/* Deconstruction allows to retrieve specific set of data from an object*/
+namespace Discards
+{
+    public class Person
+    {
+        public string FirstName { get; set; }
+        public string MiddleName { get; set; }
+        public string LastName { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+
+        // instance constructor
+        public Person(string fname, string mname, string lname,
+string cityName, string stateName)
+        {
+            FirstName = fname;
+            MiddleName = mname;
+            LastName = lname;
+            City = cityName;
+            State = stateName;
+        }
+
+        // return the first and last name
+        public void Desconstruct(out string fname, out string lname)
+        {
+            fname = FirstName;
+            lname = LastName;
+        }
+
+        public void Desconstruct(out string fname, out string mname, out string lname)
+        {
+            fname = FirstName;
+            mname = MiddleName;
+            lname = LastName;
+        }
+
+        public void Deconstruct(out string fname, out string lname,
+out string city, out string state)
+        {
+            fname = FirstName;
+            lname = LastName;
+            city = City;
+            state = State;
+        }
+    }
+    class Program
+    {
+        public static void Main()
+        {
+            var p = new Person("John", "Quincy", "Adams", "Boston", "MA");
+
+            // deconstruct
+            var (fname, _, city, _) = p;
+            Console.WriteLine($"Hello {fname} of {city}!");
+        }
+    }
+}
