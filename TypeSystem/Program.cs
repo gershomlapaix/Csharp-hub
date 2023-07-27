b7bf5398fb7b0ba392aeda052a7326294d97c6e3
gershomlapaix <nsengiyumv33@gmail.com>
2023-07-27 09:49:45 +0200

    type system

---
+++ TypeSystem/Program.cs
@@ -0,0 +1,23 @@
+﻿char firstLetter = 'C';
+var limit = 4;
+int[] source = { 0, 1, 2, 3 };
+string message = "This is a string";
+
+var query = from item in source
+            where item <= limit
+            select item;
+
+Console.WriteLine(message);
+
+// reference types
+public class Person
+{
+
+    // required: modifier that mandates to set properties as a part of new expression
+    public required string LastName { get; set; }
+    public required string FirstName { get; set; }
+}
+
+// var p1 = new Person(); // error
+// var p2 = new Person() { FirstName = "Grace", LastName = "Hopper" };
+
