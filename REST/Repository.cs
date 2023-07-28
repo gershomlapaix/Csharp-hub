f07bd075937ee777d584c86eb9324e8161d9e23d
gershomlapaix <nsengiyumv33@gmail.com>
2023-07-28 10:02:43 +0200

    finish REST example

--- REST/Repository.cs
+++ REST/Repository.cs
@@ -6,4 +6,9 @@ public record class Repository(
     [property: JsonPropertyName("description")] string Description,
 [property: JsonPropertyName("html_url")] Uri GitHubHomeUrl,
 [property: JsonPropertyName("homepage")] Uri Homepage,
-[property: JsonPropertyName("watchers")] int Watchers);
+[property: JsonPropertyName("watchers")] int Watchers,
+[property: JsonPropertyName("pushed_at")] DateTime LastPushUtc)
+{
+    // using an experession-bodied member
+    public DateTime LastPush => LastPushUtc.ToLocalTime();
+}
