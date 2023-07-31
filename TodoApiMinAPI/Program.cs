2a5b24ba1256e03a745d5b8816fbd8b148e65e4d
gershomlapaix <nsengiyumv33@gmail.com>
2023-07-31 11:22:44 +0200

    update

--- TodoApiMinAPI/Program.cs
+++ TodoApiMinAPI/Program.cs
@@ -23,8 +23,25 @@ app.MapGet("/todoitems/{id}", async (int id, TodoDb db) =>
             : Results.NotFound());
 
 //  add a new todo
-app.MapPost("/todoitems", async(Todo todo, TodoDb db)=>{
+app.MapPost("/todoitems", async (Todo todo, TodoDb db) =>
+{
     db.Todos.Add(todo);
     await db.SaveChangesAsync();
+
+    return Results.Created($"/todoitems/{todo.Id}", todo);
+});
+
+// update an existing todo
+app.MapPut("/todoitems/{id}", async (int id, Todo inputTodo, TodoDb db) =>
+{
+    var todo = await db.Todos.FindAsync(id);
+
+    if (todo is null) return Results.NotFound();
+    todo.Name = inputTodo.Name;
+    todo.IsComplete = inputTodo.IsComplete;
+
+   await db.SaveChangesAsync();
+
+    return Results.NoContent();
 });
 app.Run();
