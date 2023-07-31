5c399ef323379613b80866e4784f7f6c4b3e9273
gershomlapaix <nsengiyumv33@gmail.com>
2023-07-31 10:17:57 +0200

    get all todos api

--- TodoApiMinAPI/Program.cs
+++ TodoApiMinAPI/Program.cs
@@ -1,6 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<TodoDb>(opt => opt.UseInMemoryDatabase("TodoList"));
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/", () => "Todo Apis!");
+
+// get all todo items
+app.MapGet("/todoitems", async (TodoDb db) =>
+    await db.Todos.ToListAsync());
 
 app.Run();
