e0155180885bf9056f1ba530a24817b5af37a2ec
gershomlapaix <nsengiyumv33@gmail.com>
2023-07-31 11:07:26 +0200

    create a new todo

--- TodoApiMinAPI/Program.cs
+++ TodoApiMinAPI/Program.cs
@@ -11,4 +11,20 @@ app.MapGet("/", () => "Todo Apis!");
 app.MapGet("/todoitems", async (TodoDb db) =>
     await db.Todos.ToListAsync());
 
+// get completed todos
+app.MapGet("/todoitems/complete", async (TodoDb db) =>
+    await db.Todos.Where(t => t.IsComplete).ToListAsync());
+
+// get a one todo
+app.MapGet("/todoitems/{id}", async (int id, TodoDb db) =>
+    await db.Todos.FindAsync(id)
+        is Todo todo
+            ? Results.Ok(todo)
+            : Results.NotFound());
+
+//  add a new todo
+app.MapPost("/todoitems", async(Todo todo, TodoDb db)=>{
+    db.Todos.Add(todo);
+    await db.SaveChangesAsync();
+});
 app.Run();
--- TodoApiMinAPI/Todo.cs
+++ TodoApiMinAPI/Todo.cs
@@ -3,5 +3,5 @@ public class Todo
 {
     public int Id { get; set; }
     public string? Name { get; set; }
-    public bool isComplete { get; set; }
+    public bool IsComplete { get; set; }
 }
