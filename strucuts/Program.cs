7feab7f780bad3d1d39e58dc4b536f89b39b93d0
gershomlapaix <nsengiyumv33@gmail.com>
2023-08-25 11:44:32 +0200

    pattern matching

--- strucuts/Program.cs
+++ strucuts/Program.cs
@@ -9,5 +9,33 @@ class Program{
 
     var ms = new Measurement[2];
     Console.WriteLine(string.Join(", ", ms));
+
+    // other more
+    
+    int? maybe = 12;
+    if(maybe is int number){
+        Console.WriteLine($"The nullable int 'maybe' has the value {number}");
+    }else
+    {
+        Console.WriteLine("The nullable int 'maybe' doesn't hold a value");
+    }
+
+
+    string? message = "This is not null string";
+    if(message is not null){
+        Console.WriteLine(message);
+    }
+
+    // relational patterns
+    string WaterState(int tempInFahrenheit)=>
+    tempInFahrenheit switch{
+        (> 32) and (< 212) => "Liquid",
+         < 32 => "solid",
+        32 => "solid/liquid transition",
+        212 => "liquid / gas transition",
+        _=> "gas"
+    };
+
+    Console.WriteLine(WaterState(365));
 }
 }
