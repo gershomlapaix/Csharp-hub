173c36723f9083bac86e1bf46b8b28745d6c13ec
gershomlapaix <nsengiyumv33@gmail.com>
2023-08-25 13:31:03 +0200

    todo minimal api

--- TodoApiMinAPI/TodoDb.cs
+++
@@ -1,8 +0,0 @@
-/* Defining a database context, for coordinating "Entity Framework" */
-using Microsoft.EntityFrameworkCore;
-
-class TodoDb : DbContext
-{
-    public TodoDb(DbContextOptions<TodoDb> options) : base(options) { }
-    public DbSet<Todo> Todos => Set<Todo>();
-}
