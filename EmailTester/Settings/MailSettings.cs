845826241658e942d22feea477c26d9b10f9e365
gershomlapaix <nsengiyumv33@gmail.com>
2023-08-26 18:10:44 +0200

    email settings model

---
+++ EmailTester/Settings/MailSettings.cs
@@ -0,0 +1,8 @@
+// this class is designed to hold the data from appsettings.Development.json
+public class MailSettings{
+    private string Mail {get; set;}
+    private string DisplayName {get; set;}
+    private string Password {get; set;}
+    private string Host {get; set;}
+    private int  Port {get; set;}
+}
